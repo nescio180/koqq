74654503d6c38da6229b0528a278eddebb039d78
Gökhan Kalender <gkalender@cellular.de>
2015-01-03 20:24:17 +0100

    Update

--- app/src/main/java/de/bananaburst/kingofqueensquiz/uitils/DataBase.java
+++ app/src/main/java/de/bananaburst/kingofqueensquiz/uitils/DataBase.java
@@ -10,7 +10,8 @@ public class DataBase {
     ArrayList<Question> mQuestions = new ArrayList<>();
 
     public DataBase() {
-        mQuestions.add(new Question("","","","",""));
+        mQuestions.add(new Question("Wie heißt Douglas mit zweitem Vornamen?","Steven","Harry","Arnold","Kevin"));
+        mQuestions.add(new Question("Wie sollte Carrie eigentlich heißen?","Simone","Selma","Sandy","Verona"));
     }
 
     public Question getRandomQuestion() {
